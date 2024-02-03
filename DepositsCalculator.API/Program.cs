5244db21fd3c026ff9f7f3736d55c2c2cadd961b
Dmytro Slonchynskyi <dmytro.slonchynskyi@nixsolutions.com>
2024-02-03 19:05:13 +0200

    Git demo commit

--- DepositsCalculator.API/Program.cs
+++ DepositsCalculator.API/Program.cs
@@ -5,6 +5,8 @@ namespace DepositsCalculator.API
 {
     public class Program
     {
+
+        // Git test commit
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
